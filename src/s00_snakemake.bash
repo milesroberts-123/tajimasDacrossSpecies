1255b3c2fb45f40aaffc002214e81c33a13ecea8
Miles David Roberts <robe1195@dev-intel14.i>
2022-09-29 13:55:58 -0400

    removed conflicts from src

--- src/s00_snakemake.bash
+++ src/s00_snakemake.bash
@@ -4,11 +4,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem-per-cpu=1G
-<<<<<<< HEAD:src/s00_snakemake.bash
-=======
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=$USER@msu.edu
->>>>>>> a4cef41d38814c213708ce8aa7f0f42975ace7ea:workflow/s00_snakemake.bash
 # output information about how this job is running using bash commands
 echo "This job is running on $HOSTNAME on `date`"
 
@@ -17,8 +14,4 @@ ml -* iccifort/2020.1.217 impi/2019.7.217 snakemake/5.26.1-Python-3.8.2
 
 # submit snakemake to HPCC
 # subtract one job and one core from max to account for this submission command
-<<<<<<< HEAD:src/s00_snakemake.bash
-snakemake --cluster "sbatch --time 04:00:00" --jobs 999 --cores 511 --use-envmodules
-=======
 snakemake --cluster "sbatch --time 4-00:00:00" --jobs 999 --cores 511 --use-envmodules
->>>>>>> a4cef41d38814c213708ce8aa7f0f42975ace7ea:workflow/s00_snakemake.bash
