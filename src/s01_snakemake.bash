a3dba9c1424bf7919aa2d7caa3260008c4577b59
Miles David Roberts <robe1195@dev-intel14.i>
2022-09-30 08:43:29 -0400

    added resources up to genotype step

--- src/s01_snakemake.bash
+++ src/s01_snakemake.bash
@@ -17,7 +17,10 @@ ml -* iccifort/2020.1.217 impi/2019.7.217 snakemake/5.26.1-Python-3.8.2
 # go to workflow directory with Snakefile
 cd ../workflow
 
+# unlock snakemake if previous instance of snakemake failed
+snakemake --unlock --cores 1
+
 # submit snakemake to HPCC
 # subtract one job and one core from max to account for this submission command
 # rerun-incomplete in case previous snakemake instances failed and left incomplete files
-snakemake --cluster "sbatch --time 4-00:00:00 --partition=josephsnodes --account=josephsnodes" --jobs 999 --cores 511 --use-envmodules --rerun-incomplete
+snakemake --cluster "sbatch --time 4-00:00:00 --partition=josephsnodes --account=josephsnodes --cpus-per-task={threads} --mem-per-cpu={resources.mem_mb_per_cpu}" --jobs 999 --cores 511 --use-envmodules --rerun-incomplete
