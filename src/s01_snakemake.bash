975743b0ef7b2928a3ee4df24f8d2384633869ff
Miles David Roberts <robe1195@dev-intel14.i>
2022-09-29 20:26:54 -0400

    updated snakemake call

--- src/s01_snakemake.bash
+++ src/s01_snakemake.bash
@@ -5,12 +5,15 @@
 #SBATCH --time=7-00:00:00
 #SBATCH --mem-per-cpu=1G
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=$USER@msu.edu
+#SBATCH --mail-user=robe1195@msu.edu
 # output information about how this job is running using bash commands
 echo "This job is running on $HOSTNAME on `date`"
 
 # load snakemake
 ml -* iccifort/2020.1.217 impi/2019.7.217 snakemake/5.26.1-Python-3.8.2
+
+# go to workflow directory with Snakefile
+cd workflow
 
 # submit snakemake to HPCC
 # subtract one job and one core from max to account for this submission command
