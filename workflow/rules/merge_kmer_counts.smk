b4abead0cfaed6e070b2dbfdffc8e94ec86e7e79
Miles David Roberts <robe1195@dev-intel14.i>
2022-11-08 11:54:41 -0500

    add script to merge kmer counts

---
+++ workflow/rules/merge_kmer_counts.smk
@@ -0,0 +1,12 @@
+rule merge_kmer_counts:
+	input:
+		expand("{sampleSe}_se_kmers.txt", sampleSe=samplesSe.index) + expand("{samplePe}_pe_kmers.txt", samplePe=samplesPe.index)
+	output:
+		"merged_kmer_counts.txt"
+	threads: 50
+	resources:
+		mem_mb_per_cpu=2000
+	envmodules:
+		"iccifort/2019.5.281 impi/2018.5.288 R/4.0.0"
+	shell:
+		"Rscript scripts/merge_kmer_counts.R {input} {output}"
--- workflow/scripts/merge_kmer_counts.R
+++ workflow/scripts/merge_kmer_counts.R
@@ -1,8 +1,12 @@
+# check if parallel package is installed
+if (!require(parallel)) install.packages('parallel')
+library(parallel)
+
 # get file names passed to script as arguments
 args = commandArgs(trailingOnly=TRUE)
 
 # read key and all kmer count files into memory
-kmerCounts = lapply(args, read.table, header = F)
+kmerCounts = mclapply(args, read.table, header = F)
 
 # merge key with kmer counts
 kmerCountsMerged = Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by = "V1", all.x = TRUE), kmerCounts)
@@ -15,6 +19,25 @@ myNormalize = function(x){
 kmerCountsMergedNorm = apply(kmerCountsMerged[-1,], myNormalize, MARGIN = 2)
 
 # calculate dissimilarity between kmer profiles
-brayCurtisDissimilarity=function(x,y){
-	1 - (2*sum(pmin(x,y)))/sum(x+y)
+# x = indices of two columns/samples to compare
+# data = dataframe of normalized k-mer counts
+brayCurtisDissimilarity=function(x, data){
+  coly = data[,x[1]]
+  colz = data[,x[2]]
+  1 - (2*sum(pmin(coly,colz)))/sum(coly+colz)
 }
+
+# Get list of column pairs to loop over for calculating dissimilarity
+indices = list()
+k = 1
+for(i in 1:(ncol(kmerCountsMergedNorm) - 1)){
+  for(j in (i+1):ncol(kmerCountsMergedNorm)){
+    indices[[k]] = c(i,j)
+    k = k + 1
+  }
+}
+
+# calculate dissimilarity
+dissim = unlist(mclapply(indices, brayCurtisDissimilarity, data = kmerCountsMergedNorm))
+
+print(mean(dissim))
